ddb9365f27e11453279ded7c64d748994a7dfbda
Twilight <cz241126@live.com>
2018-07-01 12:57:11 +0800

    bugs Login

---
+++ bin/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LemonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace bin
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App() {
+            if (Directory.Exists(InfoHelper.GetPath()) == false)
+                Directory.CreateDirectory(InfoHelper.GetPath());
+        }
+    }
+}
---
+++ bin/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using LemonLibrary;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bin
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+        private int RadomPsw;
+        private void LoginBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (id.Text != "" && yzm.Text != "")
+                if (yzm.Text == RadomPsw.ToString())
+                    (Resources["Login"] as Storyboard).Begin();
+                else tb.Text = "验证码错误(oﾟvﾟ)ノ";
+        }
+
+        private async void yzm_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (id.Text != "")
+            {
+                RadomPsw = new Random().Next(10000, 99999);
+                MailMessage m = new MailMessage();
+                m.From = new MailAddress("lemon.app@qq.com", "小萌登录");
+                m.To.Add(new MailAddress(id.Text + "@qq.com"));
+                m.Subject = "小萌:您的登录验证码";
+                m.SubjectEncoding = Encoding.UTF8;
+                m.Body = RadomPsw.ToString();
+                m.BodyEncoding = Encoding.UTF8;
+                m.IsBodyHtml = true;
+                SmtpClient s = new SmtpClient();
+                s.Host = "smtp.qq.com";
+                s.Port = 587;
+                s.EnableSsl = true;
+                s.Credentials = new NetworkCredential("lemon.app@qq.com", "qtmiqibczofmddbi");
+                await s.SendMailAsync(m);
+                tb.Text = "发送成功=￣ω￣=";
+            }else tb.Text = "先输入你的企鹅号(*/ω＼*)";
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private async void Storyboard_Completed(object sender, EventArgs e)
+        {
+            string qq = id.Text;
+            if (File.Exists(InfoHelper.GetPath() + qq + ".st"))
+                Settings.LoadUSettings(Encoding.Default.GetString(Convert.FromBase64String(LemonLibrary.TextHelper.TextDecrypt(File.ReadAllText(InfoHelper.GetPath() + qq + ".st"), LemonLibrary.TextHelper.MD5.EncryptToMD5string(qq + ".st")))));
+            else LemonLibrary.Settings.SaveSettings(qq);
+            var sl = TextHelper.XtoYGetTo(await HttpHelper.GetWebAsync("http://r.pengyou.com/fcg-bin/cgi_get_portrait.fcg?uins=" + qq, Encoding.Default), "portraitCallBack(", ")", 0);
+            JObject o = JObject.Parse(sl);
+            await HttpHelper.HttpDownloadFileAsync($"http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin={qq}&spec=100", InfoHelper.GetPath() + qq + ".jpg");
+            var image = new System.Drawing.Bitmap(InfoHelper.GetPath() + qq + ".jpg");
+            TX.Background = new ImageBrush(image.ToImageSource());
+            Settings.USettings.UserName = o[qq][6].ToString();
+            Settings.USettings.UserImage = InfoHelper.GetPath() + qq + ".jpg";
+            Settings.USettings.LemonAreeunIts = qq;
+            Settings.SaveSettings();
+            Settings.LSettings.NAME = qq;
+            Settings.LSettings.RNBM = (Boolean)RM.IsChecked;
+            Settings.LSettings.TX = InfoHelper.GetPath() + qq + ".jpg";
+            Settings.SaveLoadSettings();
+            Process.Start(AppDomain.CurrentDomain.BaseDirectory + "Lemon App.exe", id.Text);
+            Environment.Exit(0);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Storyboard_Completed_1(object sender, EventArgs e)
+        {
+            var c = new Task(new Action(delegate
+            {
+                if (File.Exists(InfoHelper.GetPath() + "Settings.st"))
+                    Settings.LoadLSettings(Encoding.Default.GetString(Convert.FromBase64String(TextHelper.TextDecrypt(File.ReadAllText(InfoHelper.GetPath() + "Settings.st"), LemonLibrary.TextHelper.MD5.EncryptToMD5string("Settings.st")))));
+                else Settings.SaveLoadSettings();
+            }));
+            c.Start();
+            c.Wait();
+            if (Settings.LSettings.RNBM)
+                (Resources["Login"] as Storyboard).Begin();
+            id.Text = Settings.LSettings.NAME;
+            if (File.Exists(Settings.LSettings.TX))
+            {
+                var image = new System.Drawing.Bitmap(Settings.LSettings.TX);
+                TX.Background = new ImageBrush(image.ToImageSource());
+            }
+            RM.IsChecked = Settings.LSettings.RNBM;
+        }
+    }
+}
