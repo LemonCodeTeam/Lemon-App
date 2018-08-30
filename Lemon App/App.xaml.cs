f867673d5b8a662aa119c147d4fbff502b853592
TwilightLemon <cz241126@live.com>
2018-08-30 14:06:11 +0800

    🌷#修复bug

--- Lemon App/App.xaml.cs
+++ Lemon App/App.xaml.cs
@@ -69,16 +69,16 @@ namespace Lemon_App
             base.OnStartup(e);
         }
         public App() {
-//#if !DEBUG
+#if !DEBUG
             Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-//#endif
+#endif
             BaseApp = this;
         }
 
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            string i = "小萌账号:" + Settings.USettings.UserName + "\r\n小萌版本:"+EM + "\r\n" + ((Exception)e.ExceptionObject).Message + "\r\n 导致错误的对象名称:" + ((Exception)e.ExceptionObject).Source + "\r\n 引发异常的方法:" + ((Exception)e.ExceptionObject).TargetSite + "\r\n  帮助链接:" + ((Exception)e.ExceptionObject).HelpLink + "\r\n 调用堆:" + ((Exception)e.ExceptionObject).StackTrace;
+            string i = "小萌账号:" + Settings.USettings.LemonAreeunIts + "\r\n小萌版本:"+EM + "\r\n" + ((Exception)e.ExceptionObject).Message + "\r\n 导致错误的对象名称:" + ((Exception)e.ExceptionObject).Source + "\r\n 引发异常的方法:" + ((Exception)e.ExceptionObject).TargetSite + "\r\n  帮助链接:" + ((Exception)e.ExceptionObject).HelpLink + "\r\n 调用堆:" + ((Exception)e.ExceptionObject).StackTrace;
             FileStream fs = new FileStream(InfoHelper.GetPath() + "Log.log", FileMode.Append);
             StreamWriter sw = new StreamWriter(fs);
             sw.Write(i);
@@ -90,7 +90,7 @@ namespace Lemon_App
         private void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
-            string i = "小萌账号:" + Settings.USettings.UserName + "\r\n小萌版本:"+EM+ "\r\n" + e.Exception.Message + "\r\n 导致错误的对象名称:" + e.Exception.Source + "\r\n 引发异常的方法:" + e.Exception.TargetSite + "\r\n  帮助链接:" + e.Exception.HelpLink + "\r\n 调用堆:" + e.Exception.StackTrace;
+            string i = "小萌账号:" + Settings.USettings.LemonAreeunIts + "\r\n小萌版本:"+EM+ "\r\n" + e.Exception.Message + "\r\n 导致错误的对象名称:" + e.Exception.Source + "\r\n 引发异常的方法:" + e.Exception.TargetSite + "\r\n  帮助链接:" + e.Exception.HelpLink + "\r\n 调用堆:" + e.Exception.StackTrace;
             FileStream fs = new FileStream(InfoHelper.GetPath() + "Log.log", FileMode.Append);
             StreamWriter sw = new StreamWriter(fs);
             sw.Write(i);
