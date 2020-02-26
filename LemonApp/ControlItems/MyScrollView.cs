56518be63e55eb64b70c833bd78b88c95be9ffed
TwilightLemon <cz241126@live.com>
2020-02-26 18:59:27 +0800

    🌼 优化滚动

--- LemonApp/ControlItems/MyScrollView.cs
+++ LemonApp/ControlItems/MyScrollView.cs
@@ -11,31 +11,30 @@ namespace LemonApp
     public class MyScrollView : ScrollViewer
     {
         private int lastTimestamp = 0;
-        protected override async void OnMouseWheel(MouseWheelEventArgs e)
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
         {
-            await Task.Run(() =>
+            var sc = ScrollDirection.Up;
+            if (e.Delta > 0)
+                sc = ScrollDirection.Down;
+            int time = e.Timestamp - lastTimestamp;
+            int move = Math.Abs(e.Delta);
+            //较缓慢地滚动
+            if (time >= 80)
             {
-                var sc = ScrollDirection.Up;
-                if (e.Delta > 0)
-                    sc = ScrollDirection.Down;
-                int time = e.Timestamp - lastTimestamp;
-                int move = Math.Abs(e.Delta);
-                Console.WriteLine(e.Delta + "  ---  " + time);
-                //较缓慢地滚动
-                if (time >= 100)
-                {
-                    Dispatcher.Invoke(() => SmoothScroll(sc, move));
-                }
-                //较快地滚动
-                else if (time >= 30) {
-                    Dispatcher.Invoke(() => SmoothScrollSpeed2x(sc));
-                }
-                //适配超快滚动&触屏滚动(无需动画)
-                else Dispatcher.Invoke(() => SmoothScrollinTouch(e.Delta));
-                lastTimestamp = e.Timestamp;
-            });
+                SmoothScroll(sc, move);
+            }
+            //较快地滚动
+            else
+            {
+                //触屏滚动(无需动画)
+                if (move<20&& time <30)
+                    SmoothScrollinTouch(e.Delta);
+                else
+                    SmoothScrollSpeed2x(sc);
+            }
+            lastTimestamp = e.Timestamp;
         }
-        private void SmoothScroll(ScrollDirection direction,int move)
+        private void SmoothScroll(ScrollDirection direction, int move)
         {
             DoubleAnimation Animation = new DoubleAnimation();
             Animation.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
@@ -43,7 +42,7 @@ namespace LemonApp
                 Animation.To = VerticalOffset - move;
             else if (ScrollDirection.Up == direction)
                 Animation.To = VerticalOffset + move;
-            Animation.Duration = TimeSpan.FromMilliseconds(300);
+            Animation.Duration = TimeSpan.FromMilliseconds(500);
             BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, Animation);
         }
         bool beginning = false;
@@ -52,17 +51,19 @@ namespace LemonApp
             if (!beginning)
             {
                 DoubleAnimation Animation = new DoubleAnimation();
+                Animation.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
                 if (ScrollDirection.Down == direction)
-                    Animation.To = VerticalOffset - 200;
+                    Animation.To = VerticalOffset - 400;
                 else if (ScrollDirection.Up == direction)
-                    Animation.To = VerticalOffset + 200;
-                Animation.Duration = TimeSpan.FromMilliseconds(120);
+                    Animation.To = VerticalOffset + 400;
+                Animation.Duration = TimeSpan.FromMilliseconds(300);
                 Animation.Completed += delegate { beginning = false; };
                 beginning = true;
                 BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, Animation);
             }
         }
-        private void SmoothScrollinTouch(int Delta) {
+        private void SmoothScrollinTouch(int Delta)
+        {
             DoubleAnimation Animation = new DoubleAnimation();
             Animation.To = VerticalOffset - Delta;
             Animation.Duration = TimeSpan.FromMilliseconds(0);
