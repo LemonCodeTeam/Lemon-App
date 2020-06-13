613bcef112c5810884cf4960da9363d0200113ea
TwilightLemon <cz241126@live.com>
2020-06-13 11:32:41 +0800

    ☀[周末修bug][好吃!] 优化流畅滚动

--- LemonApp/ControlItems/MyScrollView.cs
+++ LemonApp/ControlItems/MyScrollView.cs
@@ -7,75 +7,36 @@ using System.Windows.Media.Animation;
 
 namespace LemonApp
 {
+    /// <summary>
+    /// 全局ScrollView 行为 滚动
+    /// </summary>
     public class MyScrollView : ScrollViewer
     {
-        private int lastTimestamp = 0;
+        private double LastLocation = 0;
         protected override void OnMouseWheel(MouseWheelEventArgs e)
         {
-            var sc = ScrollDirection.Up;
-            if (e.Delta > 0)
-                sc = ScrollDirection.Down;
-            int time = e.Timestamp - lastTimestamp;
-            int move = Math.Abs(e.Delta);
-            //较缓慢地滚动
-            if (time >= 80)
-            {
-                SmoothScroll(sc, move);
-            }
-            //较快地滚动
-            else
-            {
-                //触屏滚动(无需动画)
-                if (move < 20 && time < 30)
-                    SmoothScrollinTouch(e.Delta);
-                else
-                    SmoothScrollSpeed2x(sc);
-            }
-            lastTimestamp = e.Timestamp;
+            double WheelChange = e.Delta;
+            double newOffset = LastLocation - (WheelChange * 2);
+            ScrollToVerticalOffset(LastLocation);
+            if (newOffset < 0)
+                newOffset = 0;
+            if (newOffset > ScrollableHeight)
+                newOffset = ScrollableHeight;
+
+            AnimateScroll(newOffset);
+            LastLocation = newOffset;
+            e.Handled = true;
         }
-        private void SmoothScroll(ScrollDirection direction, int move)
+        private void AnimateScroll(double ToValue)
         {
+            BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, null);
             DoubleAnimation Animation = new DoubleAnimation();
             Animation.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
-            if (ScrollDirection.Down == direction)
-                Animation.To = VerticalOffset - move;
-            else if (ScrollDirection.Up == direction)
-                Animation.To = VerticalOffset + move;
-            Animation.Duration = TimeSpan.FromMilliseconds(500);
-            Timeline.SetDesiredFrameRate(Animation, 40);
+            Animation.From = VerticalOffset;
+            Animation.To = ToValue;
+            Animation.Duration = TimeSpan.FromMilliseconds(800);
+            //Timeline.SetDesiredFrameRate(Animation, 40);
             BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, Animation);
         }
-        bool beginning = false;
-        private void SmoothScrollSpeed2x(ScrollDirection direction)
-        {
-            if (!beginning)
-            {
-                DoubleAnimation Animation = new DoubleAnimation();
-                Animation.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
-                if (ScrollDirection.Down == direction)
-                    Animation.To = VerticalOffset - 400;
-                else if (ScrollDirection.Up == direction)
-                    Animation.To = VerticalOffset + 400;
-                Animation.Duration = TimeSpan.FromMilliseconds(300);
-                Timeline.SetDesiredFrameRate(Animation, 40);
-                Animation.Completed += delegate {
-                    beginning = false; 
-                };
-                beginning = true;
-                BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, Animation);
-            }
-        }
-        private void SmoothScrollinTouch(int Delta)
-        {
-            DoubleAnimation Animation = new DoubleAnimation();
-            Animation.To = VerticalOffset - Delta;
-            Animation.Duration = TimeSpan.FromMilliseconds(0);
-            BeginAnimation(UIHelper.ScrollViewerBehavior.VerticalOffsetProperty, Animation);
-        }
-    }
-
-    public enum ScrollDirection
-    {
-        Up, Down
     }
 }
--- LemonLib/Helpers/UIHelper.cs
+++ LemonLib/Helpers/UIHelper.cs
@@ -12,11 +12,6 @@ namespace LemonLib
     {
         public static class ScrollViewerBehavior
         {
-            public static readonly DependencyProperty HorizontalOffsetProperty = DependencyProperty.RegisterAttached("HorizontalOffset", typeof(double), typeof(ScrollViewerBehavior), new UIPropertyMetadata(0.0, OnHorizontalOffsetChanged));
-            public static void SetHorizontalOffset(FrameworkElement target, double value) => target.SetValue(HorizontalOffsetProperty, value);
-            public static double GetHorizontalOffset(FrameworkElement target) => (double)target.GetValue(HorizontalOffsetProperty);
-            private static void OnHorizontalOffsetChanged(DependencyObject target, DependencyPropertyChangedEventArgs e) => (target as ScrollViewer)?.ScrollToHorizontalOffset((double)e.NewValue);
-
             public static readonly DependencyProperty VerticalOffsetProperty = DependencyProperty.RegisterAttached("VerticalOffset", typeof(double), typeof(ScrollViewerBehavior), new UIPropertyMetadata(0.0, OnVerticalOffsetChanged));
             public static void SetVerticalOffset(FrameworkElement target, double value) => target.SetValue(VerticalOffsetProperty, value);
             public static double GetVerticalOffset(FrameworkElement target) => (double)target.GetValue(VerticalOffsetProperty);
