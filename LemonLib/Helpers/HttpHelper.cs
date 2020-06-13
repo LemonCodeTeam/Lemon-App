9e693845b747a9b4c1f011e9b04e320725035905
TwilightLemon <cz241126@live.com>
2020-06-13 14:30:46 +0800

    🍕[好吃]Like Hits

--- LemonLib/Helpers/HttpHelper.cs
+++ LemonLib/Helpers/HttpHelper.cs
@@ -35,6 +35,25 @@ namespace LemonLib
             var st = await sr.ReadToEndAsync();
             return st;
         }
+        public static async Task<string> GetWebWithHeaderAsync(string url)
+        {
+            HttpWebRequest hwr = (HttpWebRequest)WebRequest.Create(url);
+            hwr.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
+            hwr.Headers.Add("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6");
+            hwr.Headers.Add("cookie","__cfduid=dbb44318d9a3e9b739a80230a206d3d8f1592021136; io=xGIaLcCN2fsSC37pADUn");
+            hwr.Headers.Add("sec-ch-ua", "\"\\\\Not\\\"A;Brand\";v=\"99\", \"Chromium\";v=\"84\", \"Microsoft Edge\";v=\"84\"");
+            hwr.Headers.Add("sec-ch-ua-mobile", "?0");
+            hwr.Headers.Add("sec-fetch-dest", "document");
+            hwr.Headers.Add("sec-fetch-mode", "navigate");
+            hwr.Headers.Add("sec-fetch-site","none");
+            hwr.Headers.Add("sec-fetch-user","?1");
+            hwr.Headers.Add("upgrade-insecure-requests", "1");
+            hwr.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.38 Safari/537.36 Edg/84.0.522.15";
+            using WebResponse res = await hwr.GetResponseAsync();
+            using StreamReader sr = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+            var st = await sr.ReadToEndAsync();
+            return st;
+        }
         public static WebHeaderCollection GetWebHeader_Yeie() => new WebHeaderCollection
             {
                 { "Accept", "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01" },
