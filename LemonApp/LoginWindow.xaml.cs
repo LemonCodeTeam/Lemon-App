dc653cc1c02a05296947b5374b2cb006a7e34e92
TwilightLemon <cz241126@live.com>
2021-02-10 16:04:34 +0800

    [🥟新年快乐] 优化登录模块

--- LemonApp/LoginWindow.xaml.cs
+++ LemonApp/LoginWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using LemonLib;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
@@ -19,25 +20,46 @@ namespace LemonApp
             InitializeComponent();
             mw = m;
         }
-        public void Api_Login()
+        public async void Api_Login()
         {
-            wb.Navigate("https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https%3A//qzs.qq.com&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=33&theme=2&target=self&s_url=https%3A%2F%2Fqzs.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&hide_border=1");
+            wb.NavigationCompleted +=async delegate {
+                Console.WriteLine("", "Arrive");
+                await wb.ExecuteScriptAsync("document.getElementsByClassName(\"lay_top\")[0].remove();");
+                await wb.ExecuteScriptAsync("document.getElementsByClassName(\"page_accredit combine_page_children align\")[0].remove();");
+                await wb.ExecuteScriptAsync("document.getElementById(\"title_0\").innerText=\"登录到Lemon App\";");
+                loading.Visibility = Visibility.Collapsed;
+                wf.Visibility = Visibility.Visible;
+            };
+            wb.Source=new Uri("https://graph.qq.com/oauth2.0/show?which=Login&display=pc&response_type=code&client_id=100497308&redirect_uri=https%3A%2F%2Fy.qq.com%2Fportal%2Fwx_redirect.html%3Flogin_type%3D1%26surl%3Dhttps%253A%252F%252Fy.qq.com%252Fportal%252Fprofile.html%26use_customer_cb%3D0&state=state&display=pc");
             Topmost = true;
             Activate();
-            wb.Navigated+= Wb_Dc_Login;
+            wb.ContentLoading+= Wb_Dc_Login;
+            await wb.EnsureCoreWebView2Async();
         }
 
-        private void Wb_Dc_Login(object sender, EventArgs e)
+        private async void Wb_Dc_Login(object sender, EventArgs e)
         {
-            if (wb.Url.ToString().Contains("user.qzone.qq.com"))
+            if (wb.Source.ToString().Contains("y.qq.com/portal/profile.html"))
             {
                 Topmost = false;
                 wb.Stop();
-                string cookie = wb.Document.Cookie;
-                string qq = TextHelper.FindTextByAB(cookie, " p_uin=o", ";", 0);
-                LoginData send = new LoginData(){
+                var cookies =(await wb.CoreWebView2.CookieManager.GetCookiesAsync("https://graph.qq.com/oauth2.0/authorize"));
+                string cookie = "";
+                foreach (var i in cookies) {
+                    cookie += i.Name + "=" + i.Value + "; ";
+                }
+                var ycookies = (await wb.CoreWebView2.CookieManager.GetCookiesAsync("https://y.qq.com/portal/profile.html"));
+                string ycookie = "";
+                foreach (var i in ycookies)
+                {
+                    ycookie += i.Name + "=" + i.Value + "; ";
+                }
+                Console.WriteLine(cookie, "LoginData");
+                string qq = TextHelper.FindTextByAB(cookie + ";", "ptui_loginuin=", ";", 0);
+                LoginData send = new LoginData()
+                {
                     qq = qq,
-                    cookie = cookie
+                    cookie = ycookie
                 };
                 if (cookie.Contains("p_skey="))
                 {
@@ -50,14 +72,9 @@ namespace LemonApp
                     long g_tk = hash & 0x7fffffff;
                     send.g_tk = g_tk.ToString();
                 }
-                mw.Dispatcher.Invoke(delegate { mw.Login(send); });
-                wb.DocumentCompleted -= Wb_Dc_Login;
-            }
-            else
-            {
-                wb.Document.GetElementById("title_0").InnerText = "登录到 Lemon App";
-                loading.Visibility = Visibility.Collapsed;
-                wf.Visibility = Visibility.Visible;
+                Console.WriteLine(send.g_tk, "LOGIN G_TK");
+                   mw.Dispatcher.Invoke(delegate { mw.Login(send); });
+                wb.ContentLoading -= Wb_Dc_Login;
             }
         }
 
@@ -73,8 +90,22 @@ namespace LemonApp
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            wb.ScriptErrorsSuppressed = true;
             Api_Login();
+        }
+
+        bool downloading = false;
+        private async void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!downloading)
+            {
+                Toast.Send("正在下载WebView2运行时...");
+                downloading = true;
+                string url = "https://go.microsoft.com/fwlink/p/?LinkId=2124703";
+                string path = System.IO.Path.Combine(Settings.USettings.DownloadPath, "WebViewRuntimeSetup.exe");
+                await HttpHelper.HttpDownloadFileAsync(url, path);
+                Toast.Send("完成.");
+                Process.Start(path);
+            }
         }
     }
 }
